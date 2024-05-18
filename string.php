41e086b4bf9e56815f0aa199393bb2daa50bda4c
sharif-hosen <mdsharifkhan762@gmail.com>
2024-05-18 22:06:54 +0600

    simple Library System in PHP

--- string.php
+++ string.php
@@ -3,5 +3,5 @@ $strings=["Hello","World",'PHP',"Programming"];
 foreach($strings as $string){
     $vowelCount=preg_match_all('/[awiouAEIOU]/i',$string)?:0;
     $reverseString = strrev($string);
-    echo "Original String: $string, Vowel Count: $vowelCount, Reverse String: $reverseString <br>";
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reverseString <br>";
 } 
